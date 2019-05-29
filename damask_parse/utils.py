f01146b3fba408528dcc587901ce3ad794c47843
aplowman <adam.jp@live.co.uk>
2019-05-29 18:08:40 +0100

    Add functions for volume element validation

--- damask_parse/utils.py
+++ damask_parse/utils.py
@@ -1,6 +1,9 @@
 """`damask_parse.utils.py`"""
 
 from pathlib import Path
+import copy
+
+import numpy as np
 
 
 def zeropad(num, largest):
@@ -79,3 +82,101 @@ def get_header(path):
     return header_lines
 
 
+def validate_volume_element(volume_element):
+    """Validate the parameters of a volume element, as used in the DAMASK
+    geometry file format.
+
+    TODO: check values of optional keys.
+
+    Parameters
+    ----------
+    volume_element : dict
+
+    Returns
+    -------
+    validated_ve : dict
+        Copy of `volume_element` where array-like keys represented as lists
+        are transformed to ndarrays.
+
+    """
+
+    keys = volume_element.keys()
+
+    man_keys = ['grain_idx']
+    opt_keys = ['size', 'orientations']
+    array_keys = ['grain_idx', 'size', 'orientations']
+
+    good_keys = list(set(man_keys) | set(opt_keys))
+    missing_keys = list(set(man_keys) - set(keys))
+    bad_keys = list(set(keys) - set(good_keys))
+
+    # Check mandatory keys exist:
+    if len(missing_keys) > 0:
+        msg = ('Volume element is missing mandatory key(s): {}')
+        raise ValueError(msg.format(missing_keys))
+
+    # Check for unknown keys:
+    if len(bad_keys) > 0:
+        msg = ('Volume element contains unknown key(s): {}')
+        raise ValueError(msg.format(bad_keys))
+
+    # Transform array-like keys to ndarrays:
+    validated_ve = {}
+    for key in keys:
+        val = copy.deepcopy(volume_element[key])
+        if key in array_keys and not isinstance(val, np.ndarray):
+            val = np.array(val)
+        validated_ve.update({
+            key: val
+        })
+
+    # Check mandatory key values:
+    grain_idx = validated_ve['grain_idx']
+    if grain_idx.ndim != 3:
+        msg = ('Volume element key `grain_idx` must have dimension 3, '
+               'but actually has dimension {}.')
+        raise ValueError(msg.format(grain_idx.ndim))
+
+    return validated_ve
+
+
+def check_volume_elements_equal(vol_elem_a, vol_elem_b):
+    """Check two volume elements are equivalent.
+
+    Parameters
+    ----------
+    vol_elem_a : dict
+    vol_elem_b : dict
+
+    Returns
+    -------
+    is_equal : bool
+        True if `vol_elem_a` is equal to `vol_elem_b`. Otherwise, False.
+
+    """
+
+    array_keys = ['grain_idx', 'size', 'orientations']
+
+    vol_elem_a = validate_volume_element(vol_elem_a)
+    vol_elem_b = validate_volume_element(vol_elem_b)
+
+    # Check they have the same keys:
+    if vol_elem_a.keys() != vol_elem_b.keys():
+        return False
+
+    # Compare mandatory keys:
+    if not np.array_equal(vol_elem_a['grain_idx'], vol_elem_b['grain_idx']):
+        return False
+
+    # Compare optional keys:
+    opt_keys = ['size', 'orientations']
+    for key in opt_keys:
+        if vol_elem_a.get(key) is not None:
+            if key in array_keys:
+                if not np.array_equal(vol_elem_a[key], vol_elem_b[key]):
+                    return False
+            else:
+                if vol_elem_a[key] != vol_elem_b[key]:
+                    return False
+
+    return True
---
+++ tests/test_geom.py
@@ -0,0 +1,72 @@
+"""`test_geom.py`
+
+Tests of the functionality associated with the geometry file format that is
+used for spectral simulations in DAMASK.
+
+"""
+
+from unittest import TestCase
+
+import numpy as np
+
+from damask_parse.utils import (
+    check_volume_elements_equal, validate_volume_element
+)
+
+
+class VolumeElementTestCase(TestCase):
+    """Tests on volume element functions."""
+
+    def test_validation_missing_key(self):
+        """Test error raised on missing mandatory key."""
+
+        vol_elem = {}
+        with self.assertRaises(ValueError):
+            validate_volume_element(vol_elem)
+
+    def test_validation_unknown_key(self):
+        """Test error raised on unknown key."""
+
+        vol_elem = {
+            'grain_idx': np.random.randint(0, 9, (2, 2, 2)),
+            'bad_key': 1,
+        }
+        with self.assertRaises(ValueError):
+            validate_volume_element(vol_elem)
+
+    def test_validation_grain_idx_value(self):
+        """Test error raised on incorrect `grain_idx` dimension."""
+
+        vol_elem = {
+            'grain_idx': np.random.randint(0, 9, (2, 2))
+        }
+        with self.assertRaises(ValueError):
+            validate_volume_element(vol_elem)
+
+    def test_equality_true_grain_idx(self):
+        """Test `check_volume_elements_equal` returns True for volume elements
+        with the same `grain_idx`."""
+
+        grain_idx = np.random.randint(0, 9, (2, 2, 2))
+        vol_elem_a = {
+            'grain_idx': grain_idx,
+        }
+        vol_elem_b = {
+            'grain_idx': grain_idx,
+        }
+
+        self.assertTrue(check_volume_elements_equal(vol_elem_a, vol_elem_b))
+
+    def test_equality_false_grain_idx(self):
+        """Test `check_volume_elements_equal` returns False for volume elements
+        with the different `grain_idx`."""
+
+        grain_idx = np.random.randint(0, 9, (2, 2, 2))
+        vol_elem_a = {
+            'grain_idx': grain_idx,
+        }
+        vol_elem_b = {
+            'grain_idx': grain_idx + 1,
+        }
+
+        self.assertFalse(check_volume_elements_equal(vol_elem_a, vol_elem_b))
